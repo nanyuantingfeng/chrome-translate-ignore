d89c9f93320276d2a6f03739f2974b3b528aa7c4
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2020-06-01 14:39:10 +0800

    + First Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # chrome-translate-ignore
-chrome-translate-ignore
+
+This extension can ignore the pre, code elements when calling Google translate
---
+++ content-script.js
@@ -0,0 +1,9 @@
+﻿(function () {
+  var __IGNORE_LIST = ["PRE", "CODE"];
+
+  [].filter
+    .call(document.all, function (el) {
+      return __IGNORE_LIST.indexOf(el.tagName.toUpperCase()) > -1;
+    })
+    .forEach((el) => (el.className += " notranslate"));
+})();
