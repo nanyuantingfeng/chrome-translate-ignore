add9cebbfaf635f1d65b52476aa2fa89b138fec1
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2020-07-14 11:20:46 +0800

    + ignore github code

--- content-script.js
+++ content-script.js
@@ -3,19 +3,20 @@
     element.className += " notranslate"
   }
 
+  function toarray (arraylike) {
+    return Array.prototype.slice.apply(arraylike)
+  }
+
+
   var __IGNORE_ELEMENTS = ["PRE", "CODE", "FONT", "A"];
 
 
-
-  [].filter
-    .call(document.all, (el) => __IGNORE_ELEMENTS.indexOf(el.tagName.toUpperCase()) > -1)
+  toarray(document.all)
+    .filter((el) => __IGNORE_ELEMENTS.indexOf(el.tagName.toUpperCase()) > -1)
     .forEach(notranslate);
 
   var __IGNORE_CLASSNAME = ['highlight']
 
-  __IGNORE_CLASSNAME.forEach(k => {
-    Array.prototype.slice.apply(document.getElementsByClassName(k))
-      .forEach(notranslate);
-  })
+  __IGNORE_CLASSNAME.forEach(k => toarray(document.getElementsByClassName(k).forEach(notranslate)))
 
 })();
